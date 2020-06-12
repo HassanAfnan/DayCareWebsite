7457062acfe600c59e74f2121bce3e463a53f88c
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-12 18:36:02 +0500

    Add files via upload

---
+++ Connection.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace DayCareWebsite.Classes
+{
+    public class Connection
+    {
+        public SqlConnection con = new SqlConnection();
+        
+        public SqlConnection getConnection()
+        {
+            con = new SqlConnection(ConfigurationManager.ConnectionStrings["dayCareWebsite"].ToString());
+            con.Open();
+            return con;
+        }
+
+        public void ExecuteQuery(string query)
+        {
+            getConnection();
+            SqlCommand com = new SqlCommand(query, con);
+            com.ExecuteNonQuery();
+        }
+    }
+}
