4ad407be05fcccea2d22252b92afdd890352b34c
Afnan Hassan <50952934+HassanAfnan@users.noreply.github.com>
2020-06-14 16:18:21 +0500

    Add files via upload

---
+++ passwordRecovery.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DayCareWebsite
+{
+    public partial class passwordRecovery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Classes.Email em = new Classes.Email();
+            em.email = TextBox1.Text;
+            em.SendEmail(em);
+            Response.Write("Email sent");
+        }
+    }
+}
